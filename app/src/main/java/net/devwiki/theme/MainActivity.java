3b491c371b7d34e96e2cdacdca44fe91d5144340
zhangyazhou <zhangyazhou@xylink.com>
2018-10-29 20:46:01 +0800

    init

---
+++ app/src/main/java/net/devwiki/theme/MainActivity.java
@@ -0,0 +1,19 @@
+package net.devwiki.theme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(v -> showDialog());
+    }
+
+    private void showDialog() {
+
+    }
+}
